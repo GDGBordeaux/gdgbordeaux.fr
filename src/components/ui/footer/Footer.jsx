8c615990aa5435321793af8836679daae4962cf0
Bruno Sabot <brunosabot@gmail.com>
2018-04-19 22:08:30 +0200

    ✨ 💄  Add the footer component

---
+++ src/components/ui/footer/Footer.jsx
@@ -0,0 +1,14 @@
+// @flow
+import * as React from "react";
+
+import "./Footer.css";
+
+type Props = {
+  children: React.Node[]
+};
+
+const Footer = (props: Props): React.Node => {
+  return <div className="Footer">{props.children}</div>;
+};
+
+export default Footer;
