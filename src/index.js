3809d4811d6faa9f3d9df72a1ed206cf126b6a60
Bruno Sabot <brunosabot@gmail.com>
2018-04-19 22:20:54 +0200

    ✨ 💄  add scrolled and background image

--- src/index.js
+++ src/index.js
@@ -3,3 +3,20 @@ import registerServiceWorker from "./registerServiceWorker";
 
 Main();
 registerServiceWorker();
+
+const html = document.querySelector("html");
+const checkScroll = () => {
+  if (html.scrollTop < 20) {
+    html.classList.remove("scrolled");
+  } else {
+    html.classList.add("scrolled");
+  }
+};
+const setRandomBackground = () => {
+  const i = 1 + Math.floor(Math.random() * 4);
+  document.body.style.backgroundImage = "url(./asset/bg" + i + ".jpeg)";
+};
+
+window.addEventListener("scroll", checkScroll);
+document.addEventListener("DOMContentLoaded", checkScroll, false);
+document.addEventListener("DOMContentLoaded", setRandomBackground, false);
