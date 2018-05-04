501468c3f5279c680e9b9989d0666826bdbca80f
Bruno Sabot <brunosabot@gmail.com>
2018-05-04 17:47:10 +0200

    ✨  Randomize team members

--- src/routes/Team/Team/Team.jsx
+++ src/routes/Team/Team/Team.jsx
@@ -6,15 +6,37 @@ import PageTitle from "../../../components/ui/layout/PageTitle";
 import Container from "../../../components/ui/picture/Container";
 import StyledTeam from "../../../components/ui/picture/Team";
 
+function shuffle(array) {
+  let counter = array.length;
+
+  while (counter > 0) {
+    const index = Math.floor(Math.random() * counter);
+
+    counter--;
+
+    const temp = array[counter];
+    array[counter] = array[index];
+    array[index] = temp;
+  }
+
+  return array;
+}
+
+const team = [
+  { image: "./asset/brunosabot.jpg", name: "Bruno Sabot" },
+  { image: "./asset/sebastiendescamps.jpg", name: "Sébastien Descamps" },
+  { image: "./asset/ludovicgouyou.jpg", name: "Ludovic Gouyou" },
+  { image: "./asset/marcabouchacra.jpg", name: "Marc Abou Chacra" },
+  { image: "./asset/etiennegrandiervazeille.jpg", name: "Etienne Grandier-Vazeille" },
+];
+
 const Team = (): React.Node => (
   <React.Fragment>
     <PageTitle>Découvrez l'équipe du GDG Bordeaux</PageTitle>
     <Container>
-      <StyledTeam image="./asset/brunosabot.jpg">Bruno Sabot</StyledTeam>
-      <StyledTeam image="./asset/sebastiendescamps.jpg">Sébastien Descamps</StyledTeam>
-      <StyledTeam image="./asset/ludovicgouyou.jpg">Ludovic Gouyou</StyledTeam>
-      <StyledTeam image="./asset/marcabouchacra.jpg">Marc Abou Chacra</StyledTeam>
-      <StyledTeam image="./asset/etiennegrandiervazeille.jpg">Etienne Grandier-Vazeille</StyledTeam>
+      {shuffle(team).map(
+        member => <StyledTeam key={member.name} image={member.image}>{member.name}</StyledTeam>
+      )}
     </Container>
     <Footer />
   </React.Fragment>
